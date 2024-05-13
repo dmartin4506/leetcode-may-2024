4d23e583fb3a9bd94a52944080d943965ef57655
dmartin4506 <whywemakemusic@gmail.com>
2024-05-12 23:24:04 -0400

    adding solution code for leetcode 206

---
+++ leetcode206.py
@@ -0,0 +1,22 @@
+"""prompt is as follows:
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+"""
+
+#solution code is below:
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        return prev
